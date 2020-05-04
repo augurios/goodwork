504a99759b2248e5e9c8daf89f02cb5dca30c909
Augusto Valerio <auguvalerio@gmail.com>
2020-05-04 17:27:34 -0600

    adding spanish auth file

---
+++ resources/lang/es-CR/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'                                                                      => 'These credentials do not match our records.',
+    'throttle'                                                                    => 'Too many login attempts. Please try again in :seconds seconds.',
+    'Sensible Approach to Work & Collaboration for Software Teams'                => 'Sensible Approach to Work & Collaboration for Software Teams',
+    'Enter your email address and we’ll send you a recovery link.'                => 'Enter your email address and we’ll send you a recovery link.',
+    'Forgot your account’s password or having trouble logging into your account?' => 'Forgot your account’s password or having trouble logging into your account?',
+    'Register'                                                                    => 'Register',
+    'Login'                                                                       => 'Login',
+    'Remember Me'                                                                 => 'Remember Me',
+    'Forgot Your Password?'                                                       => 'Forgot Your Password?',
+    'Send Password Reset Link'                                                    => 'Send Password Reset Link',
+    'Reset Password'                                                              => 'Reset Password',
+];
